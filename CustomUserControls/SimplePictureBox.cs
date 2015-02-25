185c2a8f4777f101e1a84d28d93a085ffb71cf1f
azorlu <mandelbug@gmail.com>
2015-02-25 14:26:08 +0900

    Added SimplePictureBox user control and DemoForm

--- CustomUserControls/SimplePictureBox.cs
+++ CustomUserControls/SimplePictureBox.cs
@@ -15,6 +15,17 @@ namespace CustomUserControls
         public SimplePictureBox()
         {
             InitializeComponent();
+            Image = new Bitmap(@"E:\Ahmet\dev\projects\CustomUserControls\DemoApplication\Assets\microsoft_logo_2012.jpeg");
+            this.Dock = DockStyle.Fill;
+        }
+
+        public Image Image { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(Image, 0, 0, 360, 360);
+            base.OnPaint(e);
         }
     }
 }
--- DemoApplication/DemoForm.Designer.cs
+++ DemoApplication/DemoForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace CustomUserControls
+﻿namespace DemoApplication
 {
-    partial class SimplePictureBox
+    partial class DemoForm
     {
         /// <summary>
         /// Required designer variable.
@@ -20,25 +20,39 @@
             base.Dispose(disposing);
         }
 
-        #region Component Designer generated code
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify 
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.simplePictureBox = new CustomUserControls.SimplePictureBox();
             this.SuspendLayout();
             // 
-            // SimplePictureBox
+            // simplePictureBox
+            // 
+            this.simplePictureBox.Location = new System.Drawing.Point(13, 13);
+            this.simplePictureBox.Name = "simplePictureBox";
+            this.simplePictureBox.Size = new System.Drawing.Size(150, 150);
+            this.simplePictureBox.TabIndex = 0;
+            // 
+            // DemoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "SimplePictureBox";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.simplePictureBox);
+            this.Name = "DemoForm";
+            this.Text = "Demo";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private CustomUserControls.SimplePictureBox simplePictureBox;
     }
 }
+
